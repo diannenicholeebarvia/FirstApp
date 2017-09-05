eb6f89beb387ce85fc62f15c98cbca0f64cad283
diannenicholeebarvia <dianneebarvi@gmail.com>
2017-09-05 23:28:45 +0800

    Initial commit

---
+++ app/src/main/java/com/dianne/firstapp/Activity1.java
@@ -0,0 +1,23 @@
+package com.dianne.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+
+    public void nextActivity (View view) {
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
+
+
+    }
+
+}
